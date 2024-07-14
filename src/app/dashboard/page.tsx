541743adae29fa4b5976f8e34a4af86455c421ca
Hamza Elashry <98109755+rechanting@users.noreply.github.com>
2024-07-14 08:38:34 -0400

    Update page.tsx
    added server section

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,11 +1,33 @@
 import Image from "next/image";
 import { ClerkProvider } from "@clerk/nextjs";
-import SideBar from '@/components/sideBar'
+import SideBar from '@/components/sideBar';
 
 export default function Dashboard() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-        <SideBar />
+    <main>
+      <SideBar />
+      <div className="flex flex-row gap-4 p-24 ml-56 rounded-xl">
+        <div className="card bg-slate-600">
+          <div className="card-body">
+            <div className="flex flex-row gap-2 items-center"> {/* added items-center for alignment */}
+              <Image src="/rack.svg" width={20} height={20} alt="" className="w-5" /> {/* replaced img with Image component */}
+              <h2 className="card-header text-slate-200">Servers</h2>
+            </div>
+            <p className="text-content2 text-slate-200">You have 2 active servers.</p>
+          </div>
+          <div className="accordion-group accordion-group-hover rounded-lg">
+            <div className="accordion rounded-lg mt-2">
+              <input type="checkbox" id="toggle-5" className="accordion-toggle" />
+              <label htmlFor="toggle-5" className="accordion-title"> 
+                Server 1
+              </label>
+              <div className="accordion-content text-content2">
+                <div className="min-h-0 text-slate-200 flex flex-row gap-28">server info blah blah <a href="">Manage</a></div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </main>
   );
 }
